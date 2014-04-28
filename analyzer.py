661be40d00f64a40729117729b81ece85208affe
tomek <tomek@kom2>
2014-04-28 15:17:28 +0200

    added most common sequences extraction

--- analyzer.py
+++ analyzer.py
@@ -1,11 +1,40 @@
 import os, sys, json
+from config import *
+from itertools import combinations
+from collections import Counter
 
 class Analyzer:
     def __init__ (self, inputRawContent):
         self.inputContent = json.loads(inputRawContent)
 
     def analyze (self):
-        return self.inputContent
+        s = self.inputContent
+        resultList = []
+        sublists = [s[i:i+WINDOW_LEN] for i in range(len(s) - WINDOW_LEN + 1)]
+        for l in sublists:
+            for length in range(2, WINDOW_LEN + 1):
+                head = l[0]
+                tmp = combinations(l[1:], length-1)
+                tmp2 = [[head] + list(tup) for tup in tmp]
+                for sequence in tmp2:
+                    if self.timestampConditionFullfilled(sequence):
+                        tmp3 = self.extractField(sequence, 'type')
+                        resultList.append(tuple(tmp3))
+        c = Counter (resultList)
+        return self.jsonize(c.most_common(RESULT_SIZE))
+
+    def jsonize(self, data):
+        return json.dumps([{"seq":s,"count":cnt} for s,cnt in data])
+
+    def timestampConditionFullfilled(self, s):
+        timestamps = self.extractField(s, 'timestamp')
+        if max (timestamps) - min (timestamps) > MAX_TIME_DIFF:
+            return False
+        #TODO roznica miedzy kazda para
+        return True
+
+    def extractField(self, s, field):
+        return [event[field] for event in s]
 
 if __name__ == '__main__':
     filename = sys.argv[1]
